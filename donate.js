b54fa3b8dc01f2d86d93417e9153ff6a46d3c710
Awuor-Nancy <anyangnancy@gmail.com>
2023-02-25 18:19:50 +0300

    made paypal working and customized timeline

--- donate.js
+++ donate.js
@@ -4,15 +4,15 @@ paypal.Buttons({
       return actions.order.create({
         purchase_units: [{
           amount: {
-            value: '200.00'
+            value: '15.00'
           }
         }]
       });
     },
-    onApprove: function(data, actions) {
-      return actions.order.capture().then(function(details) {
-        console.log('Transaction completed by ' + details.payer.name.given_name);
-        // ADD YOUR OWN SUCCESS MESSAGE AND REDIRECT HERE
-      });
-    }
+    // onApprove: function(data, actions) {
+    //   return actions.order.capture().then(function(details) {
+    //     console.log('Transaction completed by ' + details.payer.name.given_name);
+    //     // ADD YOUR OWN SUCCESS MESSAGE AND REDIRECT HERE
+    //   });
+    // }
   }).render('#paypal-button-container');
