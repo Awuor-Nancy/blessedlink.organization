284cfd023d5ecb663f52cd601028c914390d527a
Awuor-Nancy <anyangnancy@gmail.com>
2023-02-27 16:27:14 +0300

    customized media queries

--- donate.js
+++ donate.js
@@ -1,18 +1,17 @@
 paypal.Buttons({
-    createOrder: function(data, actions) {
-// This function sets up the details of the transaction, amount and line item details
-      return actions.order.create({
-        purchase_units: [{
-          amount: {
-            value: '15.00'
-          }
-        }]
-      });
-    },
-    // onApprove: function(data, actions) {
-    //   return actions.order.capture().then(function(details) {
-    //     console.log('Transaction completed by ' + details.payer.name.given_name);
-    //     // ADD YOUR OWN SUCCESS MESSAGE AND REDIRECT HERE
-    //   });
-    // }
-  }).render('#paypal-button-container');
+  createOrder: function(data, actions) {
+    return actions.order.create({
+      purchase_units: [{
+        amount: {
+          value: '10.00'
+        }
+      }]
+    });
+  },
+  onApprove: function(data, actions) {
+    return actions.order.capture().then(function(details) {
+      console.log('Transaction completed by ' + details.payer.name.given_name);
+      // ADD YOUR OWN SUCCESS MESSAGE AND REDIRECT HERE
+    });
+  }
+}).render('#paypal-button-container');
